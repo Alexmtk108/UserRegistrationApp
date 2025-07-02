111694257bb40061aa4013c57e42278166cfe046
Mukhtarov Alexandr <87073419134@list.ru>
2025-07-02 18:38:10 +0500

    Initial Commit

---
+++ UserRegistrationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UserRegistrationApp
+//
+//  Created by Mukhtarov Alexandr on 02.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
